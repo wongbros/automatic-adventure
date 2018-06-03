a02d384b59b6a05d54207aa1542b4f2f3b6531ad
Brandon Wong <brandon@yerdle.com>
2018-06-03 14:41:04 -0500

    Fix routes. This closes #36

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Media from './components/Media';
-import Login from './components/Login';
 import Landing from './components/Landing';
+import Home from './components/Home';
 import PrivateRoute from './components/PrivateRoute';
 import Whitelist from './components/Whitelist';
 import './index.css';
@@ -12,7 +12,7 @@ const Routes = () => (
   <Router>
     <div>
       <Route exact path="/" component={Landing} />
-      <Route path="/auth" component={Login} />
+      <PrivateRoute path="/home" component={Home} />
       <PrivateRoute path="/whitelist" component={Whitelist} />
       <PrivateRoute path="/media" component={Media} />
     </div>
